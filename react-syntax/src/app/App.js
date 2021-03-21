e00445162c3771b50c5b179af588c36609940645
Bernese-Corgi <jnyng0621@gmail.com>
2021-03-21 20:24:55 +0900

    조건부 렌더링

--- react-syntax/src/app/App.js
+++ react-syntax/src/app/App.js
@@ -5,8 +5,9 @@ import './App.scss'
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="tomato" />
-      <Hello color="gold" />
+      <Hello name="react" color="tomato" isSpecial={true} />
+      {/* 조건부 렌더링의 경우 boolean 값을 명시하지 않는 경우 디폴트가 true이다. */}
+      <Hello color="gold" isRequired />
     </Wrapper>
   )
 }
--- react-syntax/src/components/Hello.js
+++ react-syntax/src/components/Hello.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-export default function Hello({ color, name }) {
+export default function Hello({ color, name, isSpecial, isRequired }) {
   // 구조 분해 할당으로 props 속성 가져오기
   // console.log(props) // props는 객체
   return (
     <div style={{
-      color: color
+      color
     }}>
+      {/* 삼항연산자 - 내용이 달라질때 사용 */}
+      {/* {isSpecial ? <b>*</b> : null} */}
+      {/* 동일한 표현: &&연산자 */}
+      {isSpecial && <b>*</b>}
+      {/* 삼항연산자 더 적절한 방식 */}
+      {isRequired ? '필수' : '선택'}
       안녕하세요
       {name}
     </div>
