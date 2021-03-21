5575a78fa94f73626902c2b37e0906a71eeedf62
Bernese-Corgi <jnyng0621@gmail.com>
2021-03-22 01:48:50 +0900

    객체 형태의 상태 업데이트

--- react-syntax/src/components/Input.js
+++ react-syntax/src/components/Input.js
@@ -1,35 +1,60 @@
 import React, { useState } from 'react'
 
 export default function Input() {
-  // useState
-  const [text, setText] = useState('')
+  // useState : 객체 형태로 상태 관리
+  // input의 상태를 객체 형태로 초기화
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+  })
+
+  // inputs의 프로퍼티를 구조 분해 할당
+  const { name, nickname } = inputs
 
   // input의 이벤트
   const onChange = (e) => {
-    // e.target : 현재 이벤트가 발생한 DOM 요소
-    // console.log(e.target) // <input />
-    // e.target.value : input의 입력 값을 가져올 수 있다.
-    setText(e.target.value)
+    // console.log(e.target.name) // name, nickname
+    // console.log(e.target.value) // 입력 값
+    const { name, value } = e.target
+
+    // 1. 객체 복사하기
+    const nextInputs = { ...inputs }
+    // 2. 특정 값 덮어쓰기
+    nextInputs[name] = value
+    // 3. 상태 업데이트 하기
+    setInputs(nextInputs)
+    // 1~3 한번에 표현
+    // setInputs({ ...inputs, [name]: value })
   }
 
   // input 값 리셋
   const onReset = () => {
-    setText('')
+    setInputs({
+      name: '',
+      nickname: '',
+    })
   }
 
   return (
     <div>
       <input
-        // input의 변경이 생길때 실행할 이벤트
+        name="name"
+        placeholder="이름"
         onChange={onChange}
-        // value 값을 현재 상태인 text로 지정
-        value={text} />
+        value={name}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button
         // button 클릭 시 input값 리셋
         onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
-        {text}
+        {name} ({nickname})
       </div>
     </div>
   )
