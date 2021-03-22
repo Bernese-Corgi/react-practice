a86f9eb670b0368ac570e08d451641451bbdb1e2
Bernese-Corgi <jnyng0621@gmail.com>
2021-03-22 16:33:07 +0900

    useRef: 변수를 계속 기억하고 싶을때 사용할 수 있다

--- react-syntax/src/app/App.js
+++ react-syntax/src/app/App.js
@@ -1,11 +1,38 @@
-import UserList from 'components/UserList'
+import React, { useRef } from 'react';
+import UserList from 'components/UserList';
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'Jinyoung',
+      email: 'jy@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'Jinjoo',
+      email: 'jj@gmail.com',
+    },
+    {
+      id: 3,
+      username: 'Jinee',
+      email: 'jn@gmail.com',
+    },
+  ];
+
+  // nextId에 저장된 4라는 값을 기억하고 싶을때 useRef를 사용할 수 있다.
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current); // 4
+    nextId.current += 1;
+  };
+
   return (
     <>
-      <UserList />
+      <UserList users={users} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-syntax/src/components/UserList.js
+++ react-syntax/src/components/UserList.js
@@ -1,43 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function User({ user }) {
   return (
     <li>
       <b>{user.username}</b>
-      <span style={{
-        color: 'salmon'
-      }}>({user.email})</span>
+      <span
+        style={{
+          color: 'salmon',
+        }}
+      >
+        ({user.email})
+      </span>
     </li>
-  )
+  );
 }
 
-export default function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'Jinyoung',
-      email: 'jy@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'Jinjoo',
-      email: 'jj@gmail.com',
-    },
-    {
-      id: 3,
-      username: 'Jinee',
-      email: 'jn@gmail.com',
-    },
-  ]
-
+export default function UserList({ users }) {
   return (
     <ul>
-      {
-        users.map(
-          // key는 고유한 값으로 넣어줘야 한다.
-          user => (<User user={user} key={user.id} />)
+      {users.map(
+        // key는 고유한 값으로 넣어줘야 한다.
+        (user) => (
+          <User user={user} key={user.id} />
         )
-      }
+      )}
     </ul>
-  )
+  );
 }
