08e45f51c8bde9f066985e9424c522531c1490c0
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-01 21:14:36 +0900

    useCallback

--- react-syntax/src/app/App.js
+++ react-syntax/src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from 'components/UserList';
 import CreateUser from 'components/CreateUser';
 
@@ -16,12 +16,18 @@ function App() {
 
   const { username, email } = inputs;
 
-  const onChange = (e) => {
-    // 상태를 바꿀 때 컴포넌트가 리렌더링된다
-    // input의 value값이 변경될 때마다 컴포넌트가 리렌더링된다.
-    const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: value });
-  };
+  // useCallback
+  const onChange = useCallback(
+    // 첫번째 매개변수 : 생성하고 싶은 함수
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({ ...inputs, [name]: value });
+    },
+    // 두번째 매개변수 : deps
+    // [inputs] - inputs가 변경될 때만 함수가 새로 생성된다.
+    // 빈 배열인 경우 - 컴포넌트가 렌더링될 때 한 번만 함수가 생성
+    [inputs]
+  );
 
   const [users, setUsers] = useState([
     {
@@ -47,7 +53,8 @@ function App() {
   // nextId에 저장된 4라는 값을 기억하고 싶을때 useRef를 사용할 수 있다.
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  // useCallback
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -65,22 +72,29 @@ function App() {
     });
     // console.log(nextId.current); // 4
     nextId.current += 1;
-  };
+    // username, email은 input에서 밖으로 빼준 값 -> 이런 값도 결국 상태이므로 deps 배열에 넣어야한다.
+  }, [email, username, users]);
 
-  const onRemove = (id) => {
-    // user의 id가 일치하는
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback(
+    (id) => {
+      // user의 id가 일치하는
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
 
   // 특정 값만 업데이트
-  const onToggle = (id) => {
-    setUsers(
-      users.map((user) =>
-        // 인수로 받은 user의 id가 일치하는 요소를 클릭하면 활성화 상태 변경
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          // 인수로 받은 user의 id가 일치하는 요소를 클릭하면 활성화 상태 변경
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
 
   // 아래와 같이 useMemo를 사용하지 않으면 input의 value값이 수정될 때마다 컴포넌트가 리렌더링된다 => 리렌더링될때마다 활성 사용자 수를 센다.
   // const count = countActiveUsers(users);
@@ -97,6 +111,8 @@ function App() {
 
   return (
     <>
+      {/* 이벤트 핸들러 함수 onChange, onCreate, onToggle은 컴포넌트가 리렌더링될때마다 함수가 생성된다. */}
+      {/* 이런 방식이 대체로 별로 문제가 없지만. 컴포넌트의 렌더링이 자주 발생하거나 렌더링할 컴포넌트가 많아지면 최적화해주는 것이 좋다. */}
       <CreateUser
         username={username}
         email={email}
