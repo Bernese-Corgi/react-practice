0311f8125dc8ce50baacbe33ec9fad007b871851
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-15 06:24:17 +0900

    react-router: URL 파라미터 설정하기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,6 @@
 import { Route } from "react-router";
 import { Link } from "react-router-dom";
+import { Profiles } from "./components";
 import { AboutPage, HomePage } from "./page";
 
 function App() {
@@ -12,9 +13,16 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/userA">userA 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/userB">userB 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={HomePage} exact={true} />
       <Route path={["/about", "/info"]/* path의 주소를 배열로 설정하면 여러 경로에서 같은 컴포넌트를 보여줄 수 있다. */} component={AboutPage} />
+      <Route path="/profile/:username"/* match.params.username 값을 통해 현재 username 값을 조회할 수 있습니다. */ component={Profiles} />
     </div>
   );
 }
