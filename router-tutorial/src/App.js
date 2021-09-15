ab157a7c2e97159870b612f88de1d48760d44e5a
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-15 10:06:02 +0900

    react-router: 서브라우트 설정하기-app에 적용하기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,6 @@
-import { Route } from "react-router";
-import { Link } from "react-router-dom";
-import { Profiles } from "./components";
-import { AboutPage, HomePage } from "./page";
+import { Route } from 'react-router';
+import { Link } from 'react-router-dom';
+import { AboutPage, HomePage, ProfilesPage } from './page';
 
 function App() {
   return (
@@ -14,15 +13,20 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/userA">userA 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/userB">userB 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <Route path="/" component={HomePage} exact={true} />
-      <Route path={["/about", "/info"]/* path의 주소를 배열로 설정하면 여러 경로에서 같은 컴포넌트를 보여줄 수 있다. */} component={AboutPage} />
-      <Route path="/profile/:username"/* match.params.username 값을 통해 현재 username 값을 조회할 수 있습니다. */ component={Profiles} />
+      <Route
+        path={
+          [
+            '/about',
+            '/info',
+          ] /* path의 주소를 배열로 설정하면 여러 경로에서 같은 컴포넌트를 보여줄 수 있다. */
+        }
+        component={AboutPage}
+      />
+      <Route path="/profiles" component={ProfilesPage} />
     </div>
   );
 }
