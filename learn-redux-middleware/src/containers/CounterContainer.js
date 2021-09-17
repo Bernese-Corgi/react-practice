f48485622c2c356348db5b2138eef7c9df386a1d
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-17 15:39:25 +0900

    learn-redux-middleware: 프로젝트에 redux actions 적용

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, connect } from 'react-redux';
 import { decrease, increase } from '../modules/counter';
 
-export default function CounterContainer() {
+const CounterContainer = ({ number, increase, decrease }) => {
+  /* hooks 사용 ----------------------------------------------------
   // redux store 안에 들어있는 상태값 가져오기
   // state.counter : reduce 이름이 counter이므로 store 안의 counter를 가져온다.
   const number = useSelector((state) => state.counter);
   //  디스패치 가져오기 : onIncrease, onDecrease 함수를 만들어서 dispatch하기 위해.
   const dispatch = useDispatch();
-
+  
   const onIncrease = () => {
     // 액션 생성함수 디스패치 : 액션을 만들고 디스패치
     dispatch(increase());
   };
-
+  
   const onDecrease = () => {
     // 액션 생성함수 디스패치 : 액션을 만들고 디스패치
     dispatch(decrease());
   };
+ ----------------------------------------------------------- */
 
   return (
-    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
   );
-}
+};
+
+/* connect 함수 사용 ----------------------------- */
+export default connect((state) => ({ number: state.counter }), {
+  increase,
+  decrease,
+})(CounterContainer);
--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,15 +1,30 @@
+import { createActions, handleActions } from 'redux-actions';
+
 // 액션 타입 선언
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
 
 // 액션 생성 함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createActions(INCREASE); // createActions는 액션 객체를 자동으로 생성해준다.
+export const decrease = createActions(DECREASE);
 
 // 초기 상태 : 초기 상태는 숫자도 가능하다.
 const initialState = 0;
 
-// 리듀서 함수
+// handleActions 함수 호출로 리듀서 생성하기
+const counter = handleActions(
+  // 첫번째 매개변수 : 각 액션에 대한 업데이트 함수
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  // 두번째 매개변수 : 초기 상태
+  initialState
+);
+
+export default counter;
+
+/* 기존 방식으로 리듀서 함수 정의 ---------------------------------------------------
 export default function counter(state = initialState, action) {
   switch (action.type) {
     // 액션 타입이 INCREASE이면 state + 1
@@ -17,8 +32,9 @@ export default function counter(state = initialState, action) {
       return state + 1;
     // 액션 타입이 DECREASE이면 state - 1
     case DECREASE:
-      return state + 2;
+      return state - 1;
     default:
       return state;
   }
 }
+--------------------------------------------------- */
